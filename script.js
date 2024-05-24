946f9e94e663352e1db381d17a80c3d8457aaf83
hannasabrynna <hannasabrynna135@gmail.com>
2024-05-24 20:46:15 -0300

    funções concluidas

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox =document.getElementById("input-box");
+const listContainer =document.getElementById("list-container");
+console.log("aqui")
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";  //Adiciona um "X"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+// Marcar como feito e deletar
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){  //Se clicar no li
+        e.target.classList.toggle("checked"); //O elemento sera marcado com checked
+        saveData();
+    }else if(e.target.tagName === "SPAN"){  //Se clicar no span (que é o "X")
+        e.target.parentElement.remove(); //Apaga elemento
+        saveData();
+    }
+}, false);
+
+// Salva do navegador
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// Mostra as Tasks quando fecharmos e abrirmos o navegador
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
